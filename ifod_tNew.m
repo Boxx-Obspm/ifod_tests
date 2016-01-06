6b6f0d4fc831cbf04795ed2e98ce319c943c0979
Boris S <boris.segret@obspm.fr>
2016-01-06 18:19:01 +0100

    temp commit

--- ifod_tNew.m
+++ ifod_tNew.m
@@ -1,8 +1,20 @@
-# ifod code's location
-# (to be coded as cmd line or default file later)
+% ifod code's location
+% (to be coded as cmd line or default file later)
 
-codePath = '../inflightOD'
-# tester le fonctionnement de time_step.m
-# - produire un jeu d'essai simple (sinon 900 pas)
-# - commenter les resulatts pour comprendre les unites
-# - sortir time_step de la release ifod car non embarquee
+codPath = '../inflightOD';
+scnPath = './';
+scnFile = strcat(scnPath, 'scenario');
+
+%% general test
+
+addpath(codPath);
+data_extraction
+% It DOES produce a result file (name prefix given in Scenario) for every timesteps in T0
+% => what are the reducted data used?
+% => validate the accuracy of the results
+
+%% tester le fonctionnement de time_step.m
+% x produire un jeu d'essai simple
+% - commenter les resultats pour comprendre les unites
+% - sortir time_step de la release ifod car non embarquee
+
